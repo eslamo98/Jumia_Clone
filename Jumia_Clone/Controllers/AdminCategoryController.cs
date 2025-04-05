9cf4e8da6c40ef5e1ae475470e037e47ea53351e
hager_hatem_ <hagerhatem24@gmail.com>
2025-04-05 23:56:52 +0200

    Category

---
+++ Jumia_Clone/Controllers/AdminCategoryController.cs
@@ -0,0 +1,34 @@
+﻿using Jumia_Clone.Models.DTOs.CategoryDTO;
+using Jumia_Clone.Repositories.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jumia_Clone.Controllers
+{
+    [Route("api/admin/[controller]")]
+    [ApiController]
+    public class AdminCategoryController : Controller
+    {
+
+        private readonly ICategoryRepository _categoryRepository;
+
+        public AdminCategoryController(ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+        }
+        // POST: api/admin/categories
+        [HttpPost("categories")]
+        public async Task<IActionResult> CreateCategory([FromBody] CreateCategoryDto categoryDto)
+        {
+            var category = await _categoryRepository.CreateCategoryAsync(categoryDto);
+            return Ok(new { success = true, message = "Category created successfully", data = category });
+        }
+
+        // PUT: api/admin/categories/{id}
+        [HttpPut("categories/{id}")]
+        public async Task<IActionResult> UpdateCategory(int id, [FromBody] UpdateCategoryDto categoryDto)
+        {
+            var category = await _categoryRepository.UpdateCategoryAsync(id, categoryDto);
+            return Ok(new { success = true, message = "Category updated successfully", data = category });
+        }
+    }
+}
---
+++ Jumia_Clone/Repositories/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,20 @@
+﻿using Jumia_Clone.Models.DTOs.CategoryDTO;
+using Jumia_Clone.Models.Entities;
+
+namespace Jumia_Clone.Repositories.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        // Method to get all categories
+        Task<IEnumerable<CategoryDto>> GetAllCategoriesAsync(bool includeInactive = false);
+
+        // Method to get a category by ID
+        Task<CategoryDto> GetCategoryByIdAsync(int id);
+
+        // Method to create a new category
+        Task<CategoryDto> CreateCategoryAsync(CreateCategoryDto categoryDto);
+
+        // Method to update an existing category
+        Task<CategoryDto> UpdateCategoryAsync(int id, UpdateCategoryDto categoryDto);
+    }
+}
