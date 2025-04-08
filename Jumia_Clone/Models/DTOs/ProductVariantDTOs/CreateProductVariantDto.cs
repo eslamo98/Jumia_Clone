5e687898156e04b394db19f278cc6ff7f7579e29
hager_hatem_ <hagerhatem24@gmail.com>
2025-04-08 23:35:13 +0200

    ProductVarient

---
+++ Jumia_Clone/Models/DTOs/ProductVariantDTOs/CreateProductVariantDto.cs
@@ -0,0 +1,14 @@
+﻿namespace Jumia_Clone.Models.DTOs.ProductVariantDTOs
+{
+    public class CreateProductVariantDto
+    {
+        public string VariantName { get; set; }
+        public decimal Price { get; set; }
+        public decimal DiscountPercentage { get; set; }
+        public int StockQuantity { get; set; }
+        public string SKU { get; set; }
+        public string VariantImageUrl { get; set; }
+        public bool IsDefault { get; set; }
+        public List<VariantAttributeDto> Attributes { get; set; }
+    }
+}
---
+++ Jumia_Clone/Models/DTOs/ProductVariantDTOs/VariantAttributeDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Jumia_Clone.Models.DTOs.ProductVariantDTOs
+{
+    public class VariantAttributeDto
+    {
+        public string AttributeName { get; set; }
+        public string AttributeValue { get; set; }
+    }
+}
