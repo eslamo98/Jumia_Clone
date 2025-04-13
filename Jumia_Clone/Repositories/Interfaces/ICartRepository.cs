f054045cad02e53bf18150801b1b72afe495888d
=FatmaMahmoud22 <fatmamhmoud20230@gmail.com>
2025-04-13 07:10:20 +0200

    test cart endpoint

--- Jumia_Clone/Repositories/Interfaces/ICartRepository.cs
+++ Jumia_Clone/Repositories/Interfaces/ICartRepository.cs
@@ -1,6 +1,7 @@
 ﻿using Jumia_Clone.Models.DTOs.CartDTOs;
 using Jumia_Clone.Models.DTOs.CartItemDtos;
 using Jumia_Clone.Models.DTOs.GeneralDTOs;
+using Jumia_Clone.Models.Entities;
 using System.Threading.Tasks;
 
 namespace Jumia_Clone.Repositories.Interfaces
@@ -8,10 +9,13 @@ namespace Jumia_Clone.Repositories.Interfaces
     public interface ICartRepository
     {
         Task<CartDto> GetOrCreateCartAsync(int customerId, PaginationDto pagination);
+        Task<CartDto> GetCartProducts(List<CartItem> cartItems,PaginationDto pagination);
         Task<CartItemDto> AddItemToCartAsync(int customerId, AddItemToCartDto addItemDto);
+        Task<bool>AddToCart(CartItem cartItem);
         Task<CartItemDto> UpdateCartItemAsync(UpdateCartItemDto updateItemDto);
         Task<bool> RemoveItemFromCartAsync(int cartItemId);
         Task<bool> ClearCartAsync(int customerId);
+        Task<CartDto> GetCartAsync(int customerId, PaginationDto pagination);
         Task<CartItemDto> GetCartItemAsync(int cartItemId);
     }
 }
